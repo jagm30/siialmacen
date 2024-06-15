8ed16e22425dcfd666dcf23332bf10d4a6b94214
jagm30 <jose.antoniosiiayuda@gmail.com>
2024-06-15 01:41:16 -0600

    calculo de cambios3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,7 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="../../dist/img/user2-160x160.png" class="user-image" alt="User Image">
               <span class="hidden-xs">@guest
                         @if (Route::has('login'))                           
                           <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -85,7 +85,7 @@
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="../../dist/img/user2-160x160.png" class="img-circle" alt="User Image">
 
                 <p>
                   @guest
@@ -120,7 +120,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img src="../../dist/img/user2-160x160.png" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           @guest
